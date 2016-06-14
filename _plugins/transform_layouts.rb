b57ee3689ef7b092a5c666d696acd7f375197bc3
Dave Lim <dave@dlimiter.net>
2016-06-14 14:13:08 +1000

    Migrate site to Jekyll blog

---
+++ _plugins/transform_layouts.rb
@@ -0,0 +1,13 @@
+module Jekyll
+
+  class Layout
+
+    alias old_initialize initialize
+
+    def initialize(*args)
+      old_initialize(*args)
+      self.content = self.transform
+      self.content
+    end
+  end
+end
